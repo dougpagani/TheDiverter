fb5bfd2687fff0ad319e6ed4c2083da287110630
Dylan Sessler <dsessler88@gmail.com>
2022-12-19 10:23:14 -0500

    Adds dev tooling to be able to log all of the webNavigation events and see what data they offer

--- web-watcher/background.js
+++ web-watcher/background.js
@@ -1,3 +1,27 @@
 chrome.webNavigation.onBeforeNavigate.addListener(
   () => {console.log('dooby')}
 )
+
+// DEV TOOL: Logging for all of the webNavigation listeners
+
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @filedescription Initializes the extension's background page.
+ */
+
+// var eventList = ['onBeforeNavigate', 'onCreatedNavigationTarget',
+//     'onCommitted', 'onCompleted', 'onDOMContentLoaded',
+//     'onErrorOccurred', 'onReferenceFragmentUpdated', 'onTabReplaced',
+//     'onHistoryStateUpdated'];
+
+// eventList.forEach(function(e) {
+//   chrome.webNavigation[e].addListener(function(data) {
+//     if (typeof data)
+//       console.log(chrome.i18n.getMessage('inHandler'), e, data);
+//     else
+//       console.error(chrome.i18n.getMessage('inHandlerError'), e);
+//   });
+// });
